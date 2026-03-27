ea295b07b7d2dc20035b274d4e76cbb361e9fc43
Olimpio Eduardo <kamuenhoeduardo@75gmail.com>
2026-03-27 18:01:05 +0100

    Get all subjects route

--- drizzle/0000_absurd_alex_wilder.sql
+++ drizzle/0000_absurd_alex_wilder.sql
@@ -4,7 +4,7 @@ CREATE TABLE "departments" (
 	"name" varchar(255) NOT NULL,
 	"description" varchar(255),
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"update_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "departments_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
@@ -15,7 +15,7 @@ CREATE TABLE "subjects" (
 	"code" varchar(50) NOT NULL,
 	"description" varchar(255),
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"update_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "subjects_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
